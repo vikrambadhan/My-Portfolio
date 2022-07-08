716d6bfdbee49819bc74c0a2902b88be910fcdc6
vikrambadhan <85417946+vikrambadhan@users.noreply.github.com>
2022-07-08 05:59:28 +0000

    Deploying to gh-pages from  @ a0ca77eea0b765841f1ffb751efe3902eb9ab5ad 🚀

---
+++ precache-manifest.0d0ffd36278ffac8269df6e5239de2ee.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7773d231921bb921b2b0603914ebc003",
+    "url": "/My-Portfolio/index.html"
+  },
+  {
+    "revision": "67a51bd2236cc5adc6df",
+    "url": "/My-Portfolio/static/css/3.ed673b97.chunk.css"
+  },
+  {
+    "revision": "f8a36afff56f349d30c7",
+    "url": "/My-Portfolio/static/css/4.0110b6a7.chunk.css"
+  },
+  {
+    "revision": "fa36efc0b35647a1dc97",
+    "url": "/My-Portfolio/static/css/main.46514163.chunk.css"
+  },
+  {
+    "revision": "3a9cc71866d13899096d",
+    "url": "/My-Portfolio/static/js/2.6e45ac63.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/My-Portfolio/static/js/2.6e45ac63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67a51bd2236cc5adc6df",
+    "url": "/My-Portfolio/static/js/3.a0dd1190.chunk.js"
+  },
+  {
+    "revision": "f8a36afff56f349d30c7",
+    "url": "/My-Portfolio/static/js/4.3855598f.chunk.js"
+  },
+  {
+    "revision": "fa36efc0b35647a1dc97",
+    "url": "/My-Portfolio/static/js/main.c217bd5b.chunk.js"
+  },
+  {
+    "revision": "e1c7e62aaf6bd94acc0e",
+    "url": "/My-Portfolio/static/js/runtime-main.3035f897.js"
+  },
+  {
+    "revision": "403258eb04d8d772ea536a296361cc4c",
+    "url": "/My-Portfolio/static/media/AWS-logo.403258eb.jpg"
+  },
+  {
+    "revision": "a3752c4bf72688b3db38d0911cf75f61",
+    "url": "/My-Portfolio/static/media/Agustina.a3752c4b.woff"
+  },
+  {
+    "revision": "847627bffd52aab57ce2e903f144e07f",
+    "url": "/My-Portfolio/static/media/Montserrat-Regular.847627bf.ttf"
+  },
+  {
+    "revision": "b68c2c349122785409e1fba87dcb483f",
+    "url": "/My-Portfolio/static/media/SFS.b68c2c34.jpg"
+  },
+  {
+    "revision": "811293b95616a3b9e43b89cae1869e34",
+    "url": "/My-Portfolio/static/media/coding-ninjas.811293b9.jpg"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/My-Portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/My-Portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "537c33631b3ffb6fb9547340c109cf85",
+    "url": "/My-Portfolio/static/media/health-app-logo.537c3363.jpg"
+  },
+  {
+    "revision": "5abdd53ee501233901ca7931f67e37c3",
+    "url": "/My-Portfolio/static/media/innefu.5abdd53e.jpg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/My-Portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "214fd8bb68adeda9d7cf266132438842",
+    "url": "/My-Portfolio/static/media/msit.214fd8bb.png"
+  },
+  {
+    "revision": "1373bede58827a68d504073b60d092ae",
+    "url": "/My-Portfolio/static/media/nyu123.1373bede.jpg"
+  },
+  {
+    "revision": "781d08f72d5f77177d9b9b9137aee940",
+    "url": "/My-Portfolio/static/media/nyuit.781d08f7.jpg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/My-Portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "4dc9ab34a197e69f98d04102f718a283",
+    "url": "/My-Portfolio/static/media/smart-photo-album.4dc9ab34.jpg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/My-Portfolio/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
